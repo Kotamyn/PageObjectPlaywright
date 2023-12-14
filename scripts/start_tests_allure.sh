45c83ddf79beee1714c532640bf11b216744ff39
Kotamyn <67268519+Kotamyn@users.noreply.github.com>
2023-12-14 16:16:57 +0300

    Update start_tests_allure.sh

--- scripts/start_tests_allure.sh
+++ scripts/start_tests_allure.sh
@@ -1,11 +1,30 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
-GENERAL_DIR=$(pwd)
-DIR_NAME=pop
-TESTS_DIR=$GENERAL_DIR/$DIR_NAME/tests
-PATH_ALLURE=$GENERAL_DIR/allure/bin/allure
-PATH_RESULT_ALLURE=$GENERAL_DIR/allure_results/
-PATH_TO_POETRY=$GENERAL_DIR/$DIR_NAME
+# Constants
+readonly GENERAL_DIR=$(pwd)
+readonly DIR_NAME=pop
+readonly TESTS_DIR=$GENERAL_DIR/$DIR_NAME/tests
+readonly PATH_ALLURE=$GENERAL_DIR/allure/bin/allure
+readonly PATH_RESULT_ALLURE=$GENERAL_DIR/allure_results/
+readonly PATH_TO_POETRY=$GENERAL_DIR/$DIR_NAME/
 
-$PATH_TO_POETRY run pytest $TESTS_DIR –alluredir=$PATH_RESULT_ALLURE
-sh $PATH_ALLURE serve –port 8080 $PATH_RESULT_ALLURE
+
+check_folder_allure_results() {
+      echo "Does the allure_results catalog exist?"
+    if [ -d "$PATH_RESULT_ALLURE" ]; then # Is there an allure_results? 
+      echo "Folder exists"
+    else
+      echo "Create directory allure_results [path=$PATH_RESULT_ALLURE]"
+      mkdir $PATH_RESULT_ALLURE
+    fi
+}
+
+main() {
+    check_folder_allure_results
+    poetry --directory $PATH_TO_POETRY run pytest $TESTS_DIR --alluredir=$PATH_RESULT_ALLURE 
+    sh $PATH_ALLURE serve –port 8080 $PATH_RESULT_ALLURE
+}
+
+# Start main function
+main
+
